e80f9d61c72408131926934ed20c94785fbe1af9
rupert-allison <rupert.allison@astro.ox.ac.uk>
2013-10-18 16:38:14 +0100

    Minor edits
    Code clean up, minor edits and reordering a few things. Added an output function which writes the chain to a file.

--- affine.py
+++ affine.py
@@ -7,30 +7,19 @@ import matplotlib.pyplot as plt
 plt.rc('text',usetex=True)
 plt.rc('font',**{'family':'serif','serif':['Computer Modern'],'size':22})
 
-D = 2			## Dimensionality of the parameter space
-a = 2.			## Step-size parameter (see arxiv:1202.3665)
-nwalkers = 100		## Number of walkers
+## User-defined settings: change me!
 
-def lnprob(x, mu, invc):
-    diff = x-mu
-    return -np.dot(diff,np.dot(invc,diff))/2.0
+D = 2						## Dimensionality of the parameter space
+a = 2.						## Step-size parameter (see arxiv:1202.3665)
+nwalkers = 50				## Number of walkers
+nmoves = 1000				## Number of steps to iterate through
+output = 'outputAI.txt'	## Output file name
 
-def density():
-	u = np.random.random()
-	return ((u*(a-1.)+1.)**2.)/a
-
-def outrange(test):
-	## A function to determine whether or not the new sample point is within
-	## the prior range.
-	outrange=False
-	for j in range(D):
-			if test[j]>prange[1][j] or test[j]<prange[0][j]: outrange=True
-	return outrange
-
-cov = np.identity(D)
-data = np.array([0. for i in range(D)])
-prange = np.array([[-10.,10.] for i in range(D)]).T
-inputstd = np.sqrt(np.array([cov[i,i] for i in range(D)]))
+cov = np.identity(D)			## Define covariance matrix of the likelihood
+inputstd = np.sqrt(np.array([cov[i,i] for i in range(D)]))	## Calc. marginalised widths
+data = np.array([0. for i in range(D)])		## Define mean of the likelihood
+prange = np.array([[-10.,10.] for i in range(D)]).T  ## Define prior range
+icov = np.linalg.inv(cov)
 
 print 'INPUT MEANS:'
 print data
@@ -39,21 +28,39 @@ print inputstd
 print 'INPUT COVMAT:'
 print cov
 
-icov = np.linalg.inv(cov)
-nmoves = 1000
-
-# Set the number of walkers.
-nwalkers = 50
 print 'Number of parameters =', D
 print 'Number of walkers =', nwalkers
 
 # Choose an initial set of positions for the walkers.
 # e.g. a small ball around the max likelihood point
-#p = np.array([[data+0.01*np.random.randn(D)*(prange[1,:]-prange[0,:]) for i in range(nwalkers)]])
+# p = np.array([[data+0.01*np.random.randn(D)*(prange[1,:]-prange[0,:]) for i in range(nwalkers)]])
 # or e.g. a small ball around (-5,-5) so we can see the migration fo the walkers. 
 p = np.array([[[-5,-5]+0.1*np.random.randn(D)*(prange[1,:]-prange[0,:]) for i in range(nwalkers)]])
 
-# Do nmoves for each walker, using the other walkers' positions to update.
+## ------------------------------------------------------ ##
+
+## Modify this function to call an arbitrary likelihood
+def lnprob(x, mu, invc):
+    diff = x-mu
+    return -np.dot(diff,np.dot(invc,diff))/2.0
+
+## Generate a random variable z to determine step-size of the walker
+def density():
+	u = np.random.random()
+	return ((u*(a-1.)+1.)**2.)/a
+
+## A function to determine whether or not the new 
+## sample point is withinthe prior range.
+def outrange(test):
+	outrange=False
+	for j in range(D):
+			if test[j]>prange[1][j] or test[j]<prange[0][j]: outrange=True
+	return outrange
+
+## ------------------------------------------------------ ##
+
+## Now let's move the walkers!
+## Do nmoves for each walker, using the other walkers' positions to update.
 move_num = 0
 for move_num in range(nmoves-1):
 	newpos = []
@@ -74,9 +81,18 @@ for move_num in range(nmoves-1):
 		if r <= q: newpos.append(y)
 		else: newpos.append(cur_walker)
 	newpos = np.array(newpos)
+	
+	## p has the shape (nmoves, nwalkers, D) 
 	p = np.append(p, [newpos], axis = 0)
-	move_num += 1
+	
+	## y has the shape (nmoves * nwalkers, D)
 	y = np.reshape(p, (np.shape(p)[0]*np.shape(p)[1], -1))
+	
+	move_num += 1
+
+## Save the positions of all walkers at each iteration
+## into an output text file as specified above
+np.savetxt(output, y, delimiter = '\t', fmt = '% .5f')
 
 ## Calculate mean position of walkers at each iteration.
 mean = np.mean(p, axis = 1)
